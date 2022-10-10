1a80b6a712e702ae0e6f4e2e6104ab9e87f4471e
Fault1err <104224037+Fault1err@users.noreply.github.com>
2022-10-10 13:10:02 +0300

    Homework 2

--- js/main.js
+++ js/main.js
@@ -1,28 +1,98 @@
-const products = [
-    { id: 1, title: 'Notebook', price: 2000 },
-    { id: 2, title: 'Mouse', price: 20 },
-    { id: 3, title: 'Keyboard', price: 200 },
-    { id: 4, title: 'Gamepad', price: 50 },
-];
-//Функция для формирования верстки каждого товара
-//Добавить в выводе изображение
-const renderProduct = (product, img = 'img/1.jpg') => {
-    return `<div class="product-item">
-    <img src ="${img}">
-                <h3>${product.title}</h3>
-                <p>${product.price}</p>
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();//рекомендация, чтобы метод был вызван в текущем классе
+        this.render();//вывод товаров на страницу
+    }
+
+    _fetchProducts() {
+        this.goods = [
+            { id: 1, title: 'Notebook', price: 2000 },
+            { id: 2, title: 'Mouse', price: 20 },
+            { id: 3, title: 'Keyboard', price: 200 },
+            { id: 4, title: 'Gamepad', price: 50 },
+        ];
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const item = new ProductItem(product);
+            block.insertAdjacentHTML("beforeend", item.render());
+            //              block.innerHTML += item.render();
+        }
+    }
+}
+
+getSum() {
+    let sum = 0;
+    this.goods.forEach(item => {
+        sum += item.price;
+    })
+}
+
+class ProductItem {
+    constructor(product, img = 'https://via.placeholder.com/200x150') {
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.img = img;
+    }
+    render() {
+        return `<div class="product-item">
+                <img src="${this.img}">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
-};
+    }
+}
 
-const renderPage = list => {
-    document.querySelector('.products_flex').innerHTML = (list.map(product => renderProduct(product))).join('');
-};
-
+let list = new ProductList();
 
 
-// const productsList = list.map(item => renderProduct(item.title, item.price));
-// console.log(productsList);
-// document.querySelector('.products').innerHTML = productsList;
+class Basket {
+    addToBasket() {
 
-renderPage(products);
+    }
+
+    removeFromBasket() {
+
+    }
+
+    modifyBasket() {
+
+    }
+
+    showQuantity() {
+
+    }
+}
+
+class itemBasket {
+    renderItemBasket() {
+
+    } //верстка элемента на странице
+}
+
+
+
+//const products = [
+//    {id: 1, title: 'Notebook', price: 2000},
+//    {id: 2, title: 'Mouse', price: 20},
+//    {id: 3, title: 'Keyboard', price: 200},
+//    {id: 4, title: 'Gamepad', price: 50},
+//];
+//
+//const renderProduct = (product,img='https://placehold.it/200x150') => {
+//    return `<div class="product-item">
+//                <img src="${img}">
+//                <h3>${product.title}</h3>
+//                <p>${product.price}</p>
+//                <button class="buy-btn">Купить</button>
+//            </div>`
+//};
+//const renderPage = list => document.querySelector('.products').innerHTML = list.map(item => renderProduct(item)).join('');
+//
+//renderPage(products);
